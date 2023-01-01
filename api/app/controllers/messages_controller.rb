e16006fe421afb737d3c5a29b15d8109e0302857
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2023-01-01 16:51:29 +0900

    リファクタ

--- api/app/controllers/messages_controller.rb
+++ api/app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: ["index"]
 
   def index
-    messages = Message.all
+    messages = Message.includes(:user, [likes: :user])
     messages_array = messages.map do |m|
       {
         id: m.id,
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
       }
     end
 
-    render json: messages_array, status: 200
+    render json: messages_array, status: :ok
   end
 
 end
