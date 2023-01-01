94fc6888a94deef557bece60dd697f8dc8ef4976
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2023-01-01 12:54:39 +0900

    アクセス制限実装

--- app-front/src/components/NavBar.vue
+++ app-front/src/components/NavBar.vue
@@ -3,6 +3,7 @@
     <div>
       <p>こんにちは、 {{ name }}さん</p>
       <p class="email">現在、{{ email }}でログイン中です</p>
+      <div class="error">{{ error }}</div>
     </div>
     <button @click="logout">ログアウト</button>
   </nav>
--- app-front/src/router/index.js
+++ app-front/src/router/index.js
@@ -1,19 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView'
 import ChatRoom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
 
+const { validate, error } = useValidate()
 // import Home from '../views/Home.vue'
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'WelcomeView' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'WelcomeView' })
+  } else {
+    next()
+  }
+
+  next()
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'ChatRoom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'WelcomeView',
-    component: WelcomeView
+    component: WelcomeView,
+    beforeEnter: noRequireAuth
   },
   {
     path: '/chatroom',
     name: 'ChatRoom',
-    component: ChatRoom
+    component: ChatRoom,
+    beforeEnter: requireAuth
   }
 ]
 
